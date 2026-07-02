27ae1f8905cd84dc5c8f500657df95b3578ed5cc
Khushi Chauhan <khushiichauhan418@gmail.com>
2026-07-02 22:26:18 +0530

    Added Move Zeros to End Algorithm

---
+++ day9_move_zeros_to_end.py
@@ -0,0 +1,13 @@
+arr = [0, 1, 0, 3, 12, 0, 5]
+
+result = []
+
+for num in arr:
+    if num != 0:
+        result.append(num)
+
+while len(result) < len(arr):
+    result.append(0)
+
+print("Original Array:", arr)
+print("After Moving Zeros:", result)
