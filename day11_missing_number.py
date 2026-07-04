831400b59e7bd10424d0222070d5a1c8f9db8838
Khushi Chauhan <khushiichauhan418@gmail.com>
2026-07-04 23:12:45 +0530

    Solved Missing Number Problem

---
+++ day11_missing_number.py
@@ -0,0 +1,7 @@
+n = int(input("Enter value of n: "))
+arr = list(map(int, input("Enter numbers: ").split()))
+
+total = n * (n + 1) // 2
+missing = total - sum(arr)
+
+print("Missing Number:", missing)
